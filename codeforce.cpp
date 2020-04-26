5e9c980e4e8e7e586ddde5c77bd1c68f0978eabe
lightning <nvsiroptics8@gmail.com>
2020-04-26 21:54:14 +0530

    second commit

---
+++ codeforce.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,r;
+        cin>>a>>b>>c>>r;
+        int x=c-r;
+        int y=c+r;
+        int temp=a;
+        int a=min(a,b);
+        int b=max(a,b)
+        cout<<"s"<<endl;
+    return 0;
+}
